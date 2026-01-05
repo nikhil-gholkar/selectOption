bee0e934b140a39fbe500eee8f0dbe05839806a6
nikhil-gholkar <nikhil.gholkar76@gmail.com>
2026-01-05 22:33:22 +0530

    toggle the display option on div click

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import { Select } from "./Select"
+
+ const options =[
+    {label:"first",value:1},
+    {label:"second",value:2},
+    {label:"third",value:3},
+    {label:"fourth",value:4},
+    {label:"fifth",value:5},
+    {label:"sixth",value:6}
+  ]
+export const App=()=>{
+
+  const[value,setValue]=useState<typeof options[0] | undefined>(options[0])
+ 
+return(
+
+  <>
+  <Select options={options} value={value} onChange={o=>setValue(o) }/>
+  </>
+)
+}
